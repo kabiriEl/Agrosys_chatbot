d9fc813780c1ab3f7eb1b5ec651737cff052cd10
kabiriEl <elmahdikabiri2030@gmail.com>
2025-05-22 13:57:13 +0200

    initialisation de repositorie

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import os
+import uvicorn
+
+from rag_utils import load_vector_store, get_answer
+from image_predictor import predict_plant_disease
+
+
+
+
+app = FastAPI()
+
+# Initialiser le retriever
+vector_store, retriever = load_vector_store()
+
+# Schéma de question texte
+class Question(BaseModel):
+    query: str
+
+# Endpoint pour chatbot texte
+@app.post("/ask")
+async def ask_question(q: Question):
+    response = get_answer(q.query, retriever)
+    return JSONResponse(
+        content={"answer": response},
+        media_type="application/json; charset=utf-8"
+    )
+
+
+
+
+@app.post("/predict-image")
+async def predict_image(file: UploadFile = File(...)):
+    return await predict_plant_disease(file)
+
+
+# Run local
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
